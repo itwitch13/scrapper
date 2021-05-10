e107d39a28f1437b42b63f9ef27b24e464bcf8d6
Blania <blaneczka96@gmail.com>
2021-05-10 13:28:43 +0200

    Change into OOP

---
+++ scrapper.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup as soup  # HTML data structure
+from urllib.request import urlopen as uReq  # Web client
+import pandas as pd
+
+
+class Scrapper:
+    def __init__(self):
+        self.page_url = "https://www.otodom.pl/sprzedaz/nowe-mieszkanie/krakow/?search%5Bregion_id%5D=6&search%5Bcity_id%5D=38"
+        self.data_page = []
+
+    def getPageData(self):
+        client = uReq(self.page_url)
+        self.data_page = soup(client.read(), "html.parser")
+        client.close()
+
+    def getFlatInfo(self):
+        offer_items = self.data_page.findAll("div", {"class": "offer-item-details"})
+        flat_locations = self.data_page.find_all("header", {"class": "offer-item-header"})
+
+        # flat_locations = offer_items.findAll("p", {"class": "text-nowrap"})
+        # flat_details = offer_items.findAll("ul", {"class": "params"})
+        for flat, offer_detail in zip(flat_locations, offer_items):
+            location = flat.find_all("p", {"class": "text-nowrap"})
+            flat_details = offer_detail.findAll("ul", {"class": "params"})
+            prizes_m = []
+            area = []
+            rooms = []
+            for details in flat_details:
+                print(details)
+                # prizes = details.find_ll("li", {"class": "offer-item-price"})
+                prizes_m.append(details.find("li", {"class": "hidden-xs offer-item-price-per-m"}))
+                area.append(details.find("li", {"class": "hidden-xs offer-item-area"}))
+                rooms.append(details.find("li", {"class": "offer-item-rooms hidden-xs"}))
+                # detail = [detail.text for detail in details]
+                # print(prizes_m, area, rooms)
+
+
+
+
+scrapper = Scrapper()
+
+scrapper.getPageData()
+scrapper.getFlatInfo()
